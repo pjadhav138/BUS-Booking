abbfa16e5006e99f9c3ddeb15f18ee8ee7cef064
Pankaj Jadhav <=>
2021-10-24 21:40:42 +0530

    listview

---
+++ app/src/main/java/com/example/practiceapp/ListItem.java
@@ -0,0 +1,4 @@
+package com.example.practiceapp;
+
+public class ListItem {
+}
---
+++ app/src/main/java/com/example/practiceapp/view_bookings.java
@@ -0,0 +1,14 @@
+package com.example.practiceapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class view_bookings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_bookings);
+    }
+}
