a1fac408b635552c5d99918591d052cae2a494fc
Pankaj Jadhav <=>
2021-10-24 20:35:50 +0530

    Font , Home Screen , review booking

---
+++ app/src/main/java/com/example/practiceapp/review_booking.java
@@ -0,0 +1,13 @@
+package com.example.practiceapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class review_booking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review_booking);
+    }
+}
